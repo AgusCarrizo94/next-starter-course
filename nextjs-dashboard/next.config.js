88eaa74154fc6602389cc0d0355dfc68e10371b6
Agustin Carrizo Del Rio <88524014+AgusCarrizo94@users.noreply.github.com>
2023-11-27 20:35:08 +0000

    Up to date

--- nextjs-dashboard/next.config.js
+++ nextjs-dashboard/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["https://opulent-umbrella-jwq9v56vv4424gr-3000.app.github.dev/"]
+        }
+    }
+};
 
 module.exports = nextConfig;
